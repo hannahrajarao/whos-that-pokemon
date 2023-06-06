f49d8480c3d0ba0256a72117c132976e154f501d
hannahrajarao <hannahrajarao@gmail.com>
2023-06-06 00:45:36 -0400

    Show all pokemon in beautiful grid

---
+++ script.js
@@ -0,0 +1,19 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+// const imgEl = document.querySelector('img');
+async function showPokemon() {
+    for(var i=1; i<=151; i++) {
+        await fetch(url+i)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.name);
+            // var divEl = document.createElement('div');
+            var imgEl = document.createElement('img');
+            imgEl.src = data.sprites.front_default;
+            // divEl.title = data.name;
+            // divEl.appendChild(imgEl);
+            document.querySelector('#pokemon-images').appendChild(imgEl);
+        });
+    }
+}
+showPokemon();
+
